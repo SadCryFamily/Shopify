e3b754482b7e0f5828fe5fde016ba3663a2e89b9
SadCryFamily <fogger.work@gmail.com>
2023-09-14 15:16:30 +0300

    Initial commit

---
+++ src/test/java/org/shop/app/TestTaskShopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shop.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestTaskShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
